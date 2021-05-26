e782584c04c545f5da4ef69ab9f6f8574c9533f7
Andy Morgan <andy@andyjm.co.uk>
2021-05-26 17:47:43 +0100

    Initial commit

---
+++ api/src/main/java/com/ajm/kafkacommander/KafkaCommanderApplication.java
@@ -0,0 +1,13 @@
+package com.ajm.kafkacommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCommanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCommanderApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/ajm/kafkacommander/KafkaCommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajm.kafkacommander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCommanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
